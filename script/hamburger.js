a61afea5c0ebcd8e3240b176bb1dc48b5fa4cc94
BokiCode <bojancodedev@gmail.com>
2023-12-03 12:38:27 +0100

    Hamburger menu fully finished, made header responsive

--- script/hamburger.js
+++ script/hamburger.js
@@ -1,1 +1,11 @@
-jQuery(document).ready(function ($) {});
+$(document).ready(function () {
+	$("#hamburgerButton").click(function () {
+		$(".header ul").slideToggle(300);
+	});
+
+	$(window).resize(function () {
+		if ($(window).width() > 768) {
+			$(".header ul").removeAttr("style");
+		}
+	});
+});
